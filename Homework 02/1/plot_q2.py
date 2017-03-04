6fbef0fa4460b481ba1726c2e56f88a9063af2da
Saeid Dadkhah <saeid.dadkhah@live.com>
2017-03-05 01:46:59 +0330

    new plots are plotted. report is updated.

--- Homework 02/1/plot_q2.py
+++ Homework 02/1/plot_q2.py
@@ -1,33 +1,59 @@
 import pandas as pd
+
+import seaborn as sns
 import matplotlib.pyplot as plt
 
-train = pd.read_csv('train.csv', header=None)
+train = pd.read_csv('train.csv', header=None, names=['1', '2', '3', '4', '5', '6', '7'])
 
-plt.subplot(231)
-plt.suptitle('Seventh grade vs other grades', fontsize=14, fontweight='bold')
-plt.plot(train[0], train[6], 'o')
-plt.xlabel('First Grade')
-plt.ylabel('Seventh Grade')
 
-plt.subplot(232)
-plt.plot(train[1], train[6], 'o')
-plt.xlabel('Second Grade')
+fig, ((ax1, ax2, ax3), (ax4, ax5, ax6)) = plt.subplots(2, 3)
 
-plt.subplot(233)
-plt.plot(train[2], train[6], 'o')
-plt.xlabel('Third Grade')
+sns.regplot(x='1', y='7', data=train, ax=ax1)
+sns.regplot(x='2', y='7', data=train, ax=ax2)
+sns.regplot(x='3', y='7', data=train, ax=ax3)
+sns.regplot(x='4', y='7', data=train, ax=ax4)
+sns.regplot(x='5', y='7', data=train, ax=ax5)
+sns.regplot(x='6', y='7', data=train, ax=ax6)
 
-plt.subplot(234)
-plt.plot(train[3], train[6], 'o')
-plt.xlabel('Forth Grade')
-plt.ylabel('Seventh Grade')
+ax1.set_xlabel('First Grade', fontweight='bold')
+ax1.set_ylabel('Seventh Grade', fontweight='bold')
+ax2.set_xlabel('Second Grade', fontweight='bold')
+ax2.set_ylabel('')
+ax3.set_xlabel('Third Grade', fontweight='bold')
+ax3.set_ylabel('')
 
-plt.subplot(235)
-plt.plot(train[4], train[6], 'o')
-plt.xlabel('Fifth Grade')
+ax4.set_xlabel('Forth Grade', fontweight='bold')
+ax4.set_ylabel('Seventh Grade', fontweight='bold')
+ax5.set_xlabel('Fifth Grade', fontweight='bold')
+ax5.set_ylabel('')
+ax6.set_xlabel('Sixth Grade', fontweight='bold')
+ax6.set_ylabel('')
 
-plt.subplot(236)
-plt.plot(train[5], train[6], 'o')
-plt.xlabel('Sixth Grade')
+fig.suptitle('Seventh grade vs other grades (with missing values)', fontsize=18, fontweight='bold')
+plt.show(fig)
 
-plt.show()
+fig, ((ax1, ax2, ax3), (ax4, ax5, ax6)) = plt.subplots(2, 3)
+
+sns.regplot(x='1', y='7', data=train.loc[train['1'].nonzero()], ax=ax1)
+sns.regplot(x='2', y='7', data=train.loc[train['2'].nonzero()], ax=ax2)
+sns.regplot(x='3', y='7', data=train.loc[train['3'].nonzero()], ax=ax3)
+sns.regplot(x='4', y='7', data=train.loc[train['4'].nonzero()], ax=ax4)
+sns.regplot(x='5', y='7', data=train.loc[train['5'].nonzero()], ax=ax5)
+sns.regplot(x='6', y='7', data=train.loc[train['6'].nonzero()], ax=ax6)
+
+ax1.set_xlabel('First Grade', fontweight='bold')
+ax1.set_ylabel('Seventh Grade', fontweight='bold')
+ax2.set_xlabel('Second Grade', fontweight='bold')
+ax2.set_ylabel('')
+ax3.set_xlabel('Third Grade', fontweight='bold')
+ax3.set_ylabel('')
+
+ax4.set_xlabel('Forth Grade', fontweight='bold')
+ax4.set_ylabel('Seventh Grade', fontweight='bold')
+ax5.set_xlabel('Fifth Grade', fontweight='bold')
+ax5.set_ylabel('')
+ax6.set_xlabel('Sixth Grade', fontweight='bold')
+ax6.set_ylabel('')
+
+fig.suptitle('Seventh grade vs other grades (without missing values)', fontsize=18, fontweight='bold')
+plt.show(fig)
